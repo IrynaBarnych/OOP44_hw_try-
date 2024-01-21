3dc0612810ed5bcd6ed540a1c1d0d7ec8a906205
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-21 22:13:52 +0200

    6. Виведіть назви кафедр і назви груп, які до них належать.

--- OOP44_hw.sql
+++ OOP44_hw.sql
@@ -1,16 +1,8 @@
--- 5. Виведіть прізвища викладачів і назви факультетів, на яких вони читають лекції.
+-- 6. Виведіть назви кафедр і назви груп, які до них належать.
 
-SELECT Faculties.FacultyName, Teachers.TeacherSurname
-FROM Lectures
-JOIN Teachers ON Lectures.teacherid = Teachers.id
-JOIN Faculties ON Lectures.FacultyName = Faculties.FacultyName;
+SELECT Departments.Name AS DepartmentName, Groups.Name AS GroupName
+FROM Groups
+JOIN Departments ON Groups.DepartmentId = Departments.Id;
 
-
--- зробила за прикладом 
---SELECT Students.student_name, Teachers.teacher_name
---FROM Courses
---JOIN Teachers ON Courses.course_name = Teachers.subject
---JOIN Students ON Courses.student_id = Students.student_id;
---код не запустився
 
 
