066b3c93a70ee0dfe5cae8c17e8ac923ef81ccfe
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-21 21:05:21 +0200

    переробила дані

--- OOP44_hw.sql
+++ OOP44_hw.sql
@@ -1,50 +1,75 @@
--- 2. Виведіть назви факультетів, фонд фінансування кафедр
--- яких перевищує фонд фінансування факультету.
+-- Створення таблиці Curators
+CREATE TABLE Curators (
+   Id SERIAL PRIMARY KEY,
+   Name VARCHAR(255) NOT NULL,
+   Surname VARCHAR(255) NOT NULL
+);
 
-SELECT
-    F.Name AS FacultyName,
-    D.Name AS DepartmentName,
-    D.Financing AS DepartmentFinancing,
-    D.Financing AS FacultyFinancing
-FROM
-    Faculties AS F
-JOIN
-    Departments AS D ON D.Id > F.Id
-WHERE
-    D.Financing > D.Financing;
-	
--- прошу звернути увагу на цю задачу, бо вдалося зробити тільки так
+INSERT INTO Curators (Name, Surname) VALUES
+    ('Олександра', 'Іванова'),
+    ('Володимир', 'Петров'),
+    ('Наталія', 'Сидорова');
 
 
+-- Створення таблиці Subjects
+CREATE TABLE Subjects (
+   Id SERIAL PRIMARY KEY,
+   Name VARCHAR(100) UNIQUE NOT NULL
+);
 
-
-
-
-
-
-
-
-
+INSERT INTO Subjects (Name) VALUES
+    ('Математика'),
+    ('Фізика'),
+    ('Інформатика'),
+    ('Мови'),
+    ('Хімія');
 
 
 
+-- Створення таблиці Lectures
+CREATE TABLE Lectures (
+   Id SERIAL PRIMARY KEY,
+   LectureRoom TEXT NOT NULL,
+   SubjectId INT REFERENCES Subjects(Id),
+   TeacherId INT REFERENCES Teachers(Id)
+);
+
+INSERT INTO Lectures (LectureRoom, SubjectId, TeacherId) VALUES
+    ('Ауд. 101', 1, 1),
+    ('Ауд. 201', 2, 2),
+    ('Ауд. 301', 3, 3),
+    ('Ауд. 102', 1, 4),
+    ('Ауд. 202', 2, 5);
 
 
+-- Створення таблиці GroupsCurators
+CREATE TABLE GroupsCurators (
+   Id SERIAL PRIMARY KEY,
+   CuratorId INT REFERENCES Curators(Id),
+   GroupId INT REFERENCES Groups(Id)
+);
+
+INSERT INTO GroupsCurators (CuratorId, GroupId) VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (1, 4),
+    (2, 5);
 
 
+-- Створення таблиці GroupsLectures
+CREATE TABLE GroupsLectures (
+   Id SERIAL PRIMARY KEY,
+   GroupId INT REFERENCES Groups(Id),
+   LectureId INT REFERENCES Lectures(Id)
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO GroupsLectures (GroupId, LectureId) VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5);
 
 
 
