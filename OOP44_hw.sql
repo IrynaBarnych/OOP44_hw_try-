ed37615b65328acf4993720aaf485a4262f61e13
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-21 20:28:54 +0200

    2. Виведіть назви факультетів, фонд фінансування кафедр яких перевищує фонд фінансування факультету.

--- OOP44_hw.sql
+++ OOP44_hw.sql
@@ -1,5 +1,51 @@
--- 1. Виведіть усі можливі пари рядків викладачів і груп.
+-- 2. Виведіть назви факультетів, фонд фінансування кафедр
+-- яких перевищує фонд фінансування факультету.
 
-SELECT Teachers.*, Groups.*
-FROM Teachers
-CROSS JOIN Groups;
+SELECT
+    F.Name AS FacultyName,
+    D.Name AS DepartmentName,
+    D.Financing AS DepartmentFinancing,
+    D.Financing AS FacultyFinancing
+FROM
+    Faculties AS F
+JOIN
+    Departments AS D ON D.Id > F.Id
+WHERE
+    D.Financing > D.Financing;
+	
+-- прошу звернути увагу на цю задачу, бо вдалося зробити тільки так
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
