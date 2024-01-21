99a93053fe0fc96524f4449f6b89819df87a5738
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-21 22:31:10 +0200

    11. Виведіть повні імена викладачів і лекції, які вони читають (назви предметів та груп). Зробіть відбір по тим лекціям, які проходять в аудиторії «B103».

--- OOP44_hw.sql
+++ OOP44_hw.sql
@@ -1,11 +1,16 @@
--- 10. Виведіть назви груп 5-го курсу, а також назви факультетів,
--- до яких вони належать.
+-- 11. Виведіть повні імена викладачів і лекції, які вони читають
+-- (назви предметів та груп). Зробіть відбір по тим лекціям,
+-- які проходять в аудиторії «B103».
 
-SELECT Groups.Name AS GroupName, Faculties.Name AS FacultyName
-FROM Groups
-JOIN Departments ON Groups.FacultyId = Departments.FacultyId
-JOIN Faculties ON Departments.FacultyId = Faculties.Id
-WHERE Groups.Year = 5;
+SELECT Teachers.Name AS TeacherName, Teachers.Surname AS TeacherSurname, 
+       Lectures.LectureRoom, Subjects.Name AS SubjectName, Groups.Name AS GroupName
+FROM Teachers
+JOIN Lectures ON Teachers.Id = Lectures.TeacherId
+JOIN Subjects ON Lectures.SubjectId = Subjects.Id
+JOIN GroupsLectures ON Lectures.Id = GroupsLectures.LectureId
+JOIN Groups ON GroupsLectures.GroupId = Groups.Id
+WHERE Lectures.LectureRoom = 'B103';
+
 
 
 
